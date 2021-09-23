8193f07cbd674f89a912ee478042f793adf036cf
fantastas <msgitara@gmail.com>
2021-09-23 17:23:36 +0800

    connected to db

--- public/app.js
+++ public/app.js
@@ -1,16 +1,29 @@
 document.addEventListener("DOMContentLoaded", event =>{
     const app = firebase.app();
-    console.log(app);
-});
+    const db = firebase.firestore();
+    const user = db.collection('users').doc('user1');  
+    
+    user.get()
+    .then(doc => {
+        const data = doc.data();
+        document.write(data.pavadinimas + '<br>');
+        document.write(data.apie)
+    }) 
+//     user.onSnapshot(doc => {                                 // real time monitorin
+   
+//             const data = doc.data();
+//             document.write(data.pavadinimas + '<br>');
+//             document.write(data.apie)
+//         }) 
 
+   }) 
 
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
-
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+function googleLogin(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then(result => {
+        const user = result.user;
+        document.write(`Hello ${user.displayName}`);
+        console.log(user);
+    })
+}
