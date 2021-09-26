800c4d6af730d1dfbfb44237f191953d3895dd12
fantastas <msgitara@gmail.com>
2021-09-26 14:30:35 +0800

    fixed window change problem with logins

--- public/app.js
+++ public/app.js
@@ -1,51 +1,22 @@
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBEV0Mw7tlDunJAxT5otEtXJ7T7I6p2I3s",
-//     authDomain: "darbas-c7094.firebaseapp.com",
-//     projectId: "darbas-c7094",
-//     storageBucket: "darbas-c7094.appspot.com",
-//     messagingSenderId: "193937865291",
-//     appId: "1:193937865291:web:3504e55a271c75dc80abce",
-//     measurementId: "G-MD0LGWX62L"
-//   };
-
-// const app = firebase.initializeApp(firebaseConfig);
-
-
-
-  document.addEventListener("DOMContentLoaded", event =>{
+document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
     const db = firebase.firestore();
-    
-    app.auth().onAuthStateChanged((user) => {
-        var not_logged_in = document.getElementById('not-logged-in');
-        var logged_in = document.getElementById('loggeg-in');
-    
-        if (user) {
-            logged_in.style.display = 'block';
-            not_logged_in.style.display = 'none';
-            document.getElementById('fbL_login_btn').style.display = 'none';
-            document.getElementById('google_login_btn').style.display = 'none';
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
-          var uid = user.uid;
-          // ...
-        } else {
-            not_logged_in.style.display = 'block';
-            logged_in.style.display = 'none';
-            document.getElementById('fbL_login_btn').style.display = 'block';
-            document.getElementById('google_login_btn').style.display = 'block';
-          // User is signed out
-          // ...
-        }
-      });
+    checkLogInStatus();
+        
 
-
-      
-
-      
 
 })
+
+function checkLogInStatus(){
+    const app = firebase.app();
+    if(window.location == 'index.html'){
+        app.auth().onAuthStateChanged(user => {
+            if(!user) {
+            window.location = 'login.html'; 
+            }
+        });
+}
+}
 
 
 function login(event){
@@ -55,30 +26,47 @@ function login(event){
     email = document.getElementById('email').value;
     password = document.getElementById('password').value;
     app.auth().signInWithEmailAndPassword(email, password)
-   
-
-    
-.then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-
-})
-.catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-});
+    .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        window.location = 'index.html';
+        // console.log(userCredential);
+     })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        document.getElementById('error').innerHTML = error.message;
+    });
 }
 
+function createAccount(event){
+    event.preventDefault();
+    const app = firebase.app();
+    email = document.getElementById('email-create').value;
+    password = document.getElementById('password-create').value;
+    app.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+}
 
 function logout(){
     const app = firebase.app();
 
     app.auth().signOut().then(() => {
-        // Sign-out successful.
+        window.location = 'login.html';
+
     }).catch((error) => {
         // An error happened.
     });
-
 }
 
 function googleLogin(){
@@ -87,29 +75,31 @@ function googleLogin(){
     .then(result => {
         const user = result.user;
         // document.write(`Hello ${user.displayName}`);
+        window.location = 'index.html';
         console.log(user);
     })
 }
 
-
 function facebookLogin(){
     var provider = new firebase.auth.FacebookAuthProvider();
     firebase
-  .auth()
-  .signInWithPopup(provider)
-  .then((result) => {
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
     /** @type {firebase.auth.OAuthCredential} */
     var credential = result.credential;
 
     // The signed-in user info.
     var user = result.user;
+    window.location = 'index.html';
+
 
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
     var accessToken = credential.accessToken;
 
     // ...
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -120,11 +110,17 @@ function facebookLogin(){
 
 
     // ...
-  });
+    });
 //   firebase.auth().signInWithRedirect(provider);
 
 
 }
+
+
+
+
+
+
 
 
 
@@ -250,3 +246,4 @@ function facebookLogin(){
 //     //     }) 
 
 // //    }) 
+
